4d5b4ca479612b6a7091deab9216282d7c5e62aa
SystemHwang <bonker44@naver.com>
2025-05-08 20:57:23 +0900

    api에 서버 경로 게이트웨이서버로 추가

--- src/main/java/HS_hrs/employee_service/Config/SwaggerConfig.java
+++ src/main/java/HS_hrs/employee_service/Config/SwaggerConfig.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @OpenAPIDefinition(
     info = @io.swagger.v3.oas.annotations.info.Info(
@@ -18,8 +20,13 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI api(){
+        Server gatewayServer = new Server();
+        gatewayServer.setUrl("http://localhost:8080");
+        gatewayServer.setDescription("Gateway Server");
+        
         return new OpenAPI()
             .components(new Components())
+            .servers(List.of(gatewayServer))  // 서버 URL 설정
             .info(new Info()
                 .title("Employee Service API")
                 .version("v1.0.0")
